a69a07784ff9ff295dbf21f6a2c7c9835bb1ea16
hybridgreen <y.yayaoye@gmail.com>
2025-10-30 22:23:53 +0900

    deploy: update CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://trailstory.vercel.app/",
+        "https://trailstory.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
