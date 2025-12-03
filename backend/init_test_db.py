7239a8d8c16ec97fb72eb785f289360042c229f1
hybridgreen <y.yayaoye@gmail.com>
2025-12-03 15:41:00 +0900

    feat(CI): Enable postgis container

--- backend/init_test_db.py
+++ backend/init_test_db.py
@@ -1,5 +1,15 @@
 from db.schema import engine, Base
 from app.config import config
+from sqlalchemy import text
+
+with engine.connect() as conn:
+    conn.execute(text("GRANT ALL PRIVILEGES ON DATABASE database_name TO username"))
+    conn.execute(text("GRANT ALL ON SCHEMA public TO admin"))
+    conn.execute(text("CREATE EXTENSION IF NOT EXISTS postgis"))
+    
+    conn.commit()
+
 
 if config.environment == "TEST":
     Base.metadata.create_all(bind=engine)
+    
